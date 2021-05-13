2502b5d06996e686168c0043129b7b507d25eddb
Yareli05 <hcym050397@hotmail.com>
2021-05-13 17:19:00 -0500

    Practica del dia 130521

---
+++ lib/src/pages/home_temp2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePageTempDos extends StatelessWidget {
+  final String opciones;
+
+  HomePageTempDos(this.opciones);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Widgets de Yareli Campuzano'),
+      ),
+      body: Center(
+          child: Text(
+        'El numero que eligio fue $opciones',
+        style: TextStyle(fontSize: 25.0),
+      )),
+    );
+  }
+}
